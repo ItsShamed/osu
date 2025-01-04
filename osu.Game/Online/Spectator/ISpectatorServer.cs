c333636e8d534271586a23b1b86af3b74a7e2fd2
tsrk. <tsrk@tsrk.me>
2025-01-04 01:56:56 +0100

    refactor(online: spectator): make spectator system aware of other spectators
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- osu.Game/Online/Spectator/ISpectatorServer.cs
+++ osu.Game/Online/Spectator/ISpectatorServer.cs
@@ -2,6 +2,8 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Threading.Tasks;
+using osu.Game.Online.Rooms;
+using osu.Game.Screens.Play;
 
 namespace osu.Game.Online.Spectator
 {
@@ -34,7 +36,25 @@ namespace osu.Game.Online.Spectator
         /// For offline users, a subscription will be created and data will begin streaming on next play.
         /// </summary>
         /// <param name="userId">The user to subscribe to.</param>
-        Task StartWatchingUser(int userId);
+        /// <returns>The watch group of that user.</returns>
+        /// <remarks>May be <c>null</c> if this is invoked on an old server that is not returning any watch group.</remarks>
+        Task<SpectatorWatchGroup?> StartWatchingUser(int userId); // nullability can be removed 20250703
+
+        /// <summary>
+        /// Announce to spectators and specified spectated user, if the local user has loaded into a <see cref="SpectatorPlayer"/> and can see the gameplay.
+        /// </summary>
+        /// <param name="userId">The spectated user.</param>
+        /// <param name="hasLoaded">If the current user see the gameplay.</param>
+        /// <returns></returns>
+        Task SendLoadingState(int userId, bool hasLoaded);
+
+        /// <summary>
+        /// Announce to spectators and specified spectated user a new beatmap availability.
+        /// </summary>
+        /// <param name="userId">The spectated user.</param>
+        /// <param name="beatmapAvailability">The new beatmap availability</param>
+        /// <returns></returns>
+        Task SendBeatmapAvailability(int userId, BeatmapAvailability beatmapAvailability);
 
         /// <summary>
         /// Stop requesting spectating data for the specified user. Unsubscribes from receiving further data.
---
+++ osu.Game/Online/Spectator/SpectatorWatchGroup.cs
@@ -0,0 +1,34 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using MessagePack;
+
+namespace osu.Game.Online.Spectator
+{
+    /// <summary>
+    /// A group of users watching the same user.
+    /// </summary>
+    [Serializable]
+    [MessagePackObject]
+    public class SpectatorWatchGroup
+    {
+        /// <summary>
+        /// The ID of the user who is being watched.
+        /// </summary>
+        [Key(0)]
+        public readonly int UserID;
+
+        /// <summary>
+        /// The list of <see cref="SpectatorUser"/>s in this group.
+        /// </summary>
+        [Key(1)]
+        public IList<SpectatorUser> Spectators { get; set; } = new List<SpectatorUser>();
+
+        public SpectatorWatchGroup(int userID)
+        {
+            UserID = userID;
+        }
+    }
+}
