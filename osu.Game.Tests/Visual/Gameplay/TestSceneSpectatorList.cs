dff2348f79a76d4844bfbe3b9fbea5cb70b901f3
tsrk. <tsrk@tsrk.me>
2025-01-05 02:01:43 +0100

    feat(screens: play: HUD): create base spectator list component
    Signed-off-by: tsrk. <tsrk@tsrk.me>

---
+++ osu.Game.Tests/Visual/Gameplay/TestSceneSpectatorList.cs
@@ -0,0 +1,217 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
+using osu.Framework.Utils;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
+using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Online.Rooms;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Scoring;
+using osu.Game.Screens.Play;
+using osu.Game.Screens.Play.HUD;
+using osu.Game.Tests.Gameplay;
+using osu.Game.Tests.Visual.Spectator;
+
+namespace osu.Game.Tests.Visual.Gameplay
+{
+    public partial class TestSceneSpectatorList : SpectatorTestScene
+    {
+        private SpectatorList spectatorList = null!;
+
+        protected override Container<Drawable> Content { get; }
+
+        [Resolved]
+        private OsuColour colours { get; set; } = null!;
+
+        public TestSceneSpectatorList()
+        {
+            var score = new Score
+            {
+                ScoreInfo =
+                {
+                    User = new APIUser
+                    {
+                        Id = SPECTATED_ID,
+                        Username = "Spectated user"
+                    }
+                }
+            };
+
+            var gameplayState1 = TestGameplayState.Create(new OsuRuleset(), score: score);
+            base.Content.Add(new DependencyProvidingContainer
+            {
+                CachedDependencies = [(typeof(GameplayState), gameplayState1)],
+                RelativeSizeAxes = Axes.Both,
+                Child = Content = new Container
+                {
+                    RelativeSizeAxes = Axes.Both
+                }
+            });
+        }
+
+        public override void SetUpSteps()
+        {
+            base.SetUpSteps();
+
+            AddStep("setup components", () => Child = spectatorList = new SpectatorList
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+            });
+        }
+
+        [Test]
+        public void TestFirstWatcherSingleUser()
+        {
+            Watch();
+
+            AddSpectator(42);
+
+            SetBeatmapAvailability(42, BeatmapAvailability.NotDownloaded());
+            checkItemColour(42, colours.RedLight);
+
+            SetBeatmapAvailability(42, BeatmapAvailability.Downloading(0.32f));
+            checkItemColour(42, colours.Blue);
+            checkItemSpinner(42, Visibility.Hidden);
+
+            SetBeatmapAvailability(42, BeatmapAvailability.Importing());
+            checkItemColour(42, colours.Yellow);
+
+            SetBeatmapAvailability(42, BeatmapAvailability.LocallyAvailable());
+            checkItemColour(42, Colour4.White);
+            checkItemSpinner(42);
+
+            SetLoadingState(42, true);
+            checkItemSpinner(42, Visibility.Hidden);
+
+            StopWatching();
+        }
+
+        [Test]
+        public void TestFirstWatcherSorting()
+        {
+            ignoreSpectatorLimit();
+            Watch();
+
+            AddSpectatorsRange(20, 45);
+
+            AddStep("change states", () =>
+            {
+                for (int i = 20; i < 45; i++)
+                {
+                    switch (i % 5)
+                    {
+                        case 1:
+                            SpectatorClient.ChangeSpectatorBeatmapAvailability(i, SPECTATED_ID, BeatmapAvailability.NotDownloaded());
+                            break;
+
+                        case 2:
+                            SpectatorClient.ChangeSpectatorBeatmapAvailability(i, SPECTATED_ID, BeatmapAvailability.Downloading(RNG.NextSingle(0.99f)));
+                            break;
+
+                        case 3:
+                            SpectatorClient.ChangeSpectatorBeatmapAvailability(i, SPECTATED_ID, BeatmapAvailability.Importing());
+                            break;
+
+                        case 4:
+                            SpectatorClient.ChangeSpectatorBeatmapAvailability(i, SPECTATED_ID, BeatmapAvailability.LocallyAvailable());
+                            break;
+                    }
+                }
+            });
+
+            checkSorted();
+
+            StopWatching();
+        }
+
+        [Test]
+        public void TestPreSeededWatchGroupSingleUser()
+        {
+            AddSpectator(20);
+            SetBeatmapAvailability(20, BeatmapAvailability.NotDownloaded());
+
+            Watch();
+
+            checkItemPresent(20);
+            checkItemColour(20, colours.RedLight);
+
+            StopWatching();
+        }
+
+        [Test]
+        public void TestSpectatorLimit()
+        {
+            AddSpectatorsRange(20, 30);
+
+            Watch();
+
+            AddAssert("flow is visible", () => getItems().IsPresent, () => Is.True);
+
+            AddSpectator(31);
+            AddAssert("flow is not visible", () => getItems().IsPresent, () => Is.False);
+
+            RemoveSpectator(31);
+            AddAssert("flow is visible", () => getItems().IsPresent, () => Is.True);
+
+            StopWatching();
+        }
+
+        private void ignoreSpectatorLimit() => AddStep("ignore spectator limit", () => spectatorList.IgnoreSpectatorLimit = true);
+
+        private void checkSorted() => AddAssert("users are sorted", () =>
+        {
+            var flow = getItems();
+            var groups = flow.GroupBy(si => si.BeatmapAvailability.State)
+                             .OrderBy(g => g.Key)
+                             .ToList();
+
+            for (int i = 0; i < groups.Count; i++)
+            {
+                var layoutOrderedItems = groups[i].OrderBy(si => getItems().GetLayoutPosition(si)).ToList();
+                var idOrderedItems = groups[i].OrderBy(si => si.UserID).ToList();
+
+                // Layout should have the same ordering as `OrderBy(userId)`
+                if (!layoutOrderedItems.SequenceEqual(idOrderedItems))
+                    return false;
+
+                if (i + 1 >= groups.Count)
+                    continue;
+
+                var itemLayoutPositions = groups[i].Select(si => flow.GetLayoutPosition(si)).ToList();
+                var nextItemLayoutPositions = groups[i + 1].Select(si => flow.GetLayoutPosition(si)).ToList();
+
+                // Any random item from each group should respect the ordering by state
+                return itemLayoutPositions[RNG.Next(itemLayoutPositions.Count)] > nextItemLayoutPositions[RNG.Next(nextItemLayoutPositions.Count)];
+            }
+
+            return true;
+        });
+
+        private void checkItemSpinner(int userId, Visibility visibility = Visibility.Visible)
+            => AddAssert($"spinner is {visibility} for user {userId}",
+                () => getItemOf(userId)?.ChildrenOfType<LoadingSpinner>().Single().State.Value, () => Is.EqualTo(visibility));
+
+        private void checkItemColour(int userId, ColourInfo colour)
+            => AddAssert($"user {userId} has colour {colour}",
+                () => getItemOf(userId)?.ChildrenOfType<OsuSpriteText>().Single().Colour, () => Is.EqualTo(colour));
+
+        private void checkItemPresent(int userId)
+            => AddAssert($"user {userId} is present", () => getItemOf(userId), () => Is.Not.Null);
+
+        private FillFlowContainer<SpectatorList.SpectatorListItem> getItems()
+            => spectatorList.Children.OfType<FillFlowContainer<SpectatorList.SpectatorListItem>>().Single();
+
+        private SpectatorList.SpectatorListItem? getItemOf(int userId)
+            => getItems().SingleOrDefault(i => i.UserID == userId);
+    }
+}
---
+++ osu.Game/Screens/Play/HUD/SpectatorList.cs
@@ -0,0 +1,362 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Caching;
+using osu.Framework.Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Database;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
+using osu.Game.Online;
+using osu.Game.Online.API;
+using osu.Game.Online.Rooms;
+using osu.Game.Online.Spectator;
+using osuTK;
+
+namespace osu.Game.Screens.Play.HUD
+{
+    public partial class SpectatorList : VisibilityContainer
+    {
+        private const int max_spectators = 10;
+
+        private readonly Cached sorting = new Cached();
+
+        private readonly OsuSpriteText spectatorHeader;
+        private readonly FillFlowContainer<SpectatorListItem> flow;
+
+        private bool populated;
+
+        public bool IgnoreSpectatorLimit { private get; set; }
+
+        [Resolved]
+        private SpectatorClient? spectatorClient { get; set; }
+
+        [Resolved]
+        private IAPIProvider api { get; set; } = null!;
+
+        [Resolved]
+        private GameplayState? state { get; set; }
+
+        public SpectatorList()
+        {
+            AutoSizeAxes = Axes.Both;
+            InternalChildren = new Drawable[]
+            {
+                spectatorHeader = new OsuSpriteText
+                {
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                    Font = OsuFont.Torus.With(size: 18, weight: FontWeight.SemiBold),
+                    Text = "Spectators (0)",
+                },
+                flow = new FillFlowContainer<SpectatorListItem>
+                {
+                    AutoSizeAxes = Axes.Both,
+                    Y = 18,
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                    Direction = FillDirection.Vertical,
+                    LayoutDuration = 450,
+                    LayoutEasing = Easing.OutQuint,
+                }
+            };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
+        {
+            spectatorHeader.Colour = colours.Blue0;
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            if (spectatorClient == null)
+                return;
+
+            spectatorClient.OnUserBeganWatching += addSpectator;
+            spectatorClient.OnUserStoppedWatching += removeSpectator;
+            spectatorClient.OnWatchGroupChanged += _ => updateState();
+        }
+
+        protected override void PopIn()
+        {
+            this.FadeInFromZero(250);
+        }
+
+        protected override void PopOut()
+        {
+            this.FadeOutFromOne(250);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (!sorting.IsValid)
+                sort();
+        }
+
+        protected virtual int TrackedUserId => state?.Score.ScoreInfo.User.Id ?? api.LocalUser.Value.Id;
+
+        private void sort()
+        {
+            if (sorting.IsValid)
+                return;
+
+            var orderedItems = flow.OrderByDescending(i => i.BeatmapAvailability.State)
+                                   .ThenBy(i => i.UserID)
+                                   .ToList();
+
+            for (int i = 0; i < flow.Count; i++)
+                flow.SetLayoutPosition(orderedItems[i], i);
+
+            sorting.Validate();
+        }
+
+        private void addSpectator(SpectatorUser user, int watched)
+        {
+            if (watched != TrackedUserId)
+                return;
+
+            flow.Add(new SpectatorListItem(user, this));
+            spectatorHeader.Text = $"Spectators ({spectatorClient?.GetSpectators(TrackedUserId)?.Spectators.Count ?? 0})";
+
+            sorting.Invalidate();
+            updateVisibility();
+        }
+
+        private void removeSpectator(SpectatorUser user, int watched)
+        {
+            if (watched != TrackedUserId)
+                return;
+
+            flow.SingleOrDefault(i => i.UserID == user.UserID)?.PopOut();
+            spectatorHeader.Text = $"Spectators ({spectatorClient?.GetSpectators(TrackedUserId)?.Spectators.Count ?? 0})";
+
+            sorting.Invalidate();
+            updateVisibility();
+        }
+
+        private void updateState()
+        {
+            if (spectatorClient == null)
+                return;
+
+            var watchGroup = spectatorClient.GetSpectators(TrackedUserId);
+
+            // In case of a disconnect, always depopulate
+            if (watchGroup == null)
+            {
+                foreach (var listItem in flow)
+                    listItem.PopOut();
+                spectatorHeader.Text = "Spectators (0)";
+                updateVisibility();
+                populated = false;
+                return;
+            }
+
+            if (populated)
+                return;
+
+            foreach (var spectatorUser in watchGroup.Spectators)
+                flow.Add(new SpectatorListItem(spectatorUser, this));
+
+            spectatorHeader.Text = $"Spectators ({watchGroup.Spectators.Count})";
+            populated = true;
+            sorting.Invalidate();
+            updateVisibility();
+        }
+
+        private void updateVisibility()
+        {
+            if (spectatorClient?.GetSpectators(TrackedUserId)?.Spectators.Count > 0)
+                Show();
+            else
+                Hide();
+
+            if (!IgnoreSpectatorLimit && spectatorClient?.GetSpectators(TrackedUserId)?.Spectators.Count > max_spectators)
+                collapseList();
+            else
+                expandList();
+        }
+
+        private void collapseList() => flow.FadeOutFromOne(100);
+        private void expandList() => flow.FadeInFromZero(100);
+
+        public partial class SpectatorListItem : CompositeDrawable
+        {
+            public readonly int UserID;
+
+            public bool HasLoaded { get; private set; }
+            public BeatmapAvailability BeatmapAvailability { get; private set; }
+            private readonly SpectatorList list;
+            private readonly LoadingSpinner spinner;
+
+            private readonly Cached userState = new Cached();
+
+            private string username;
+
+            private readonly OsuSpriteText text;
+
+            [Resolved]
+            private OsuColour colours { get; set; } = null!;
+
+            [Resolved]
+            private UserLookupCache userLookupCache { get; set; } = null!;
+
+            [Resolved]
+            private SpectatorClient? spectatorClient { get; set; }
+
+            public SpectatorListItem(SpectatorUser user, SpectatorList list)
+            {
+                this.list = list;
+                UserID = user.UserID;
+
+                if (user.User == null)
+                {
+                    username = "loading...";
+                    resolveUsername(user.UserID);
+                }
+                else
+                    username = user.User.Username;
+
+                HasLoaded = user.HasLoaded;
+                BeatmapAvailability = user.BeatmapAvailability;
+
+                Name = $"Spectator list item \"{username}\" ({UserID})";
+
+                Alpha = 0;
+                Scale = new Vector2(0.5f);
+                AutoSizeAxes = Axes.Both;
+
+                InternalChild = new FillFlowContainer
+                {
+                    Direction = FillDirection.Horizontal,
+                    AutoSizeAxes = Axes.Both,
+                    Spacing = new Vector2(6),
+                    Children = new Drawable[]
+                    {
+                        text = new OsuSpriteText
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            Text = username,
+                        },
+                        spinner = new LoadingSpinner
+                        {
+                            Size = new Vector2(10),
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            State = { Value = Visibility.Hidden }
+                        }
+                    },
+                };
+            }
+
+            public void PopOut()
+            {
+                this.FadeOutFromOne(450f, Easing.OutQuint)
+                    .ScaleTo(0.5f, 450f, Easing.OutQuint)
+                    .Then().Expire();
+            }
+
+            protected override void LoadComplete()
+            {
+                base.LoadComplete();
+
+                this.FadeInFromZero(0.45, Easing.OutQuint).ScaleTo(1, 0.45, Easing.OutQuint);
+
+                if (spectatorClient != null)
+                    spectatorClient.OnUserChangedState += onUserChangedState;
+            }
+
+            protected override void Update()
+            {
+                base.Update();
+
+                if (!userState.IsValid)
+                    updateState();
+            }
+
+            private void onUserChangedState(SpectatorUser user, int watched)
+            {
+                if (watched != list.TrackedUserId)
+                    return;
+
+                if (user.UserID != UserID)
+                    return;
+
+                HasLoaded = user.HasLoaded;
+                BeatmapAvailability = user.BeatmapAvailability;
+
+                userState.Invalidate();
+                list.sorting.Invalidate();
+            }
+
+            private void resolveUsername(int userId) => Scheduler.AddOnce(() =>
+            {
+                userLookupCache.GetUserAsync(userId).ContinueWith(t =>
+                {
+                    if (t.IsCompleted)
+                    {
+                        string? resolvedUsername = t.GetResultSafely()?.Username;
+
+                        if (resolvedUsername != null)
+                        {
+                            Schedule(() => username = resolvedUsername);
+                            return;
+                        }
+                    }
+
+                    Schedule(() => username = $"[unresolved {userId}]");
+                });
+            });
+
+            private void updateState()
+            {
+                const double text_fading_time = 100f;
+
+                switch (BeatmapAvailability.State)
+                {
+                    case DownloadState.Unknown:
+                        text.FadeColour(colours.Gray5, text_fading_time).FadeTo(0.75f, text_fading_time);
+                        text.Text = username;
+                        break;
+
+                    case DownloadState.NotDownloaded:
+                        text.FadeColour(colours.RedLight, text_fading_time).FadeTo(1f, text_fading_time);
+                        text.Text = username;
+                        break;
+
+                    case DownloadState.Downloading:
+                        text.FadeColour(colours.Blue, text_fading_time).FadeTo(0.90f, text_fading_time);
+                        text.Text = $"{username} ({BeatmapAvailability.DownloadProgress:0.0%})";
+                        break;
+
+                    case DownloadState.Importing:
+                        text.FadeColour(colours.Yellow, text_fading_time).FadeTo(1f, text_fading_time);
+                        text.Text = username;
+                        break;
+
+                    case DownloadState.LocallyAvailable:
+                        text.FadeColour(Colour4.White, text_fading_time).FadeTo(1f, text_fading_time);
+                        text.Text = username;
+                        break;
+                }
+
+                if (BeatmapAvailability.State == DownloadState.LocallyAvailable && !HasLoaded)
+                    spinner.Show();
+                else
+                    spinner.Hide();
+
+                userState.Validate();
+            }
+        }
+    }
+}
