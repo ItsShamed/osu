21cd30d955abcc676a9a13229ead9f2aab3cd90e
tsrk. <tsrk@tsrk.me>
2025-01-12 17:08:38 +0100

    feat(localisation): add support for `SoloSpectatorScreen`
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- osu.Game/Localisation/SpectatorStrings.cs
+++ osu.Game/Localisation/SpectatorStrings.cs
@@ -14,6 +14,26 @@ namespace osu.Game.Localisation
         /// </summary>
         public static LocalisableString SpectatorListTitle(int count = 0) => new TranslatableString(getKey(@"spectator_list_title"), @"Spectators ({0})", count);
 
+        /// <summary>
+        /// "Another player is also watching"
+        /// </summary>
+        public static LocalisableString OtherSpectatorSingle => new TranslatableString(getKey(@"other_spectator_single"), @"Another player is also watching");
+
+        /// <summary>
+        /// "{0} other players are also watching"
+        /// </summary>
+        public static LocalisableString OtherSpectatorsMultiple(int count) => new TranslatableString(getKey(@"other_spectators_multiple"), @"{0} other players are also watching", count);
+
+        /// <summary>
+        /// "Spectator Mode"
+        /// </summary>
+        public static LocalisableString SpectatorMode => new TranslatableString(getKey(@"spectator_mode"), @"Spectator Mode");
+
+        /// <summary>
+        /// "Start Watching"
+        /// </summary>
+        public static LocalisableString StartWatching => new TranslatableString(getKey(@"start_watching"), @"Start Watching");
+
         private static string getKey(string key) => $@"{prefix}:{key}";
     }
 }
