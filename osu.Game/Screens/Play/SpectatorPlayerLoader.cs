93475699b73f323fa65e5843bd0c6174c2248ac9
tsrk. <tsrk@tsrk.me>
2025-01-12 01:29:31 +0100

    feat(spectator): send availability and load states
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- osu.Game/Screens/Play/SpectatorPlayerLoader.cs
+++ osu.Game/Screens/Play/SpectatorPlayerLoader.cs
@@ -2,7 +2,10 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using osu.Framework.Allocation;
 using osu.Framework.Screens;
+using osu.Game.Online.Rooms;
+using osu.Game.Online.Spectator;
 using osu.Game.Scoring;
 
 namespace osu.Game.Screens.Play
@@ -10,6 +13,9 @@ namespace osu.Game.Screens.Play
     public partial class SpectatorPlayerLoader : PlayerLoader
     {
         public readonly ScoreInfo Score;
+
+        [Resolved]
+        private SpectatorClient spectatorClient { get; set; } = null!;
 
         public SpectatorPlayerLoader(Score score, Func<SpectatorPlayer> createPlayer)
             : base(createPlayer)
@@ -26,7 +32,16 @@ namespace osu.Game.Screens.Play
             Mods.Value = Score.Mods;
             Ruleset.Value = Score.Ruleset;
 
+            spectatorClient.UpdateBeatmapAvailability(Score.UserID, BeatmapAvailability.LocallyAvailable());
+            spectatorClient.UpdateLoadingState(Score.UserID, false);
+
             base.OnEntering(e);
+        }
+
+        protected override void ContentOut()
+        {
+            base.ContentOut();
+            spectatorClient.UpdateLoadingState(Score.UserID, true);
         }
     }
 }
